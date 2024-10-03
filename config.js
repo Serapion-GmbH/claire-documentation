86ea2e7e1237cd40ff9d3d214f4e1d247d6939ed
TeufikUmihanic <52312458+TeufikUmihanic@users.noreply.github.com>
2024-10-03 15:44:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   gatsby: {
-    pathPrefix: '',
+    pathPrefix: '/nova-documentation',
     siteUrl: 'https://serapion-gmbh.github.io',
     gaTrackingId: null,
     trailingSlash: false,
