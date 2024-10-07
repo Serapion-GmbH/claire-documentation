06a9ed5d8db636cea943d4f980fec2c46d5c392b
Teufik Umihanic <teufik.umihanic@serapion.net>
2024-10-07 10:00:32 +0200

    update gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,10 @@
 const componentWithMDXScope = require('gatsby-plugin-mdx/component-with-mdx-scope');
-
 const path = require('path');
-
 const startCase = require('lodash.startcase');
-
 const config = require('./config');
+
+// Prefix to be added to the URLs
+const pathPrefix = config.gatsby.pathPrefix;
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
@@ -35,10 +35,10 @@ exports.createPages = ({ graphql, actions }) => {
           reject(result.errors);
         }
 
-        // Create blog posts pages.
+        // Create pages for MDX nodes
         result.data.allMdx.edges.forEach(({ node }) => {
           createPage({
-            path: node.fields.slug ? node.fields.slug : '/',
+            path: `${pathPrefix}${node.fields.slug ? node.fields.slug : '/'}`, // Ensure slug includes the pathPrefix
             component: path.resolve('./src/templates/docs.js'),
             context: {
               id: node.fields.id,
@@ -56,7 +56,7 @@ exports.onCreateWebpackConfig = ({ actions }) => {
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
       alias: {
         $components: path.resolve(__dirname, 'src/components'),
-        buble: '@philpl/buble', // to reduce bundle size
+        buble: '@philpl/buble', // Reduce bundle size
       },
     },
   });
@@ -73,26 +73,22 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 
   if (node.internal.type === `Mdx`) {
     const parent = getNode(node.parent);
-
     let value = parent.relativePath.replace(parent.ext, '');
 
     if (value === 'index') {
       value = '';
     }
 
-    if (config.gatsby && config.gatsby.trailingSlash) {
-      createNodeField({
-        name: `slug`,
-        node,
-        value: value === '' ? `/` : `/${value}/`,
-      });
-    } else {
-      createNodeField({
-        name: `slug`,
-        node,
-        value: `/${value}`,
-      });
-    }
+    // Adjust slug creation to account for pathPrefix
+    const slugValue = config.gatsby && config.gatsby.trailingSlash
+      ? value === '' ? `/` : `/${value}/`
+      : `/${value}`;
+
+    createNodeField({
+      name: `slug`,
+      node,
+      value: `${pathPrefix}${slugValue}`, // Prepend the pathPrefix to the slug
+    });
 
     createNodeField({
       name: 'id',
